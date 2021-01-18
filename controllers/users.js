b9233d13bd7dd189c7acc857ca946a02ef13c03a
Centon <nicolas.centon@gmail.com>
2021-01-18 21:06:51 +0400

    ajout de users.js dans controllers

---
+++ controllers/users.js
@@ -0,0 +1,17 @@
+import User from "../models/user";
+import jwt from "jsonwebtoken";
+import bcrypt from "bcryptjs";
+export const signUp = (req, res) => {
+  const user = new User({
+    username: req.body.username,
+    email: req.body.email,
+    password: bcrypt.hashSync(req.body.password, 8)
+  });
+  user.save((err, user) => {
+    if (err) {
+      res.status(500).send({ message: err });
+      return;
+    }
+    res.send({ message: "User was registered successfully!" });
+  });
+};

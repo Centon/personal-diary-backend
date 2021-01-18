d70fb9465d1ebff026bede6a585076531e348a27
Centon <nicolas.centon@gmail.com>
2021-01-18 21:25:00 +0400

    ajout updateNote

--- controllers/notes.js
+++ controllers/notes.js
@@ -28,3 +28,46 @@ const createNote = (req, res) => {
         });
     });
 };
+//deuxiéme partie
+const updateNote = (req, res) => {
+    if (!req.body) {
+      return res.status(400).send({
+        message: "Data to update can not be empty!"
+      });
+    }
+    const id = req.params.id;
+    Note.findByIdAndUpdate(id, req.body, { useFindAndModify: false })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot update note with id=${id}. Note was not found!`
+          });
+        } else
+          res.send({ message: "Note was updated successfully!" });
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: `Error occurred while updating note with id=${id}.`
+        });
+      });
+  };
+  const deleteNote = (req, res) => {
+    const id = req.params.id;
+    Note.findByIdAndRemove(id, { useFindAndModify: false })
+      .then(data => {
+        if (!data) {
+          res.status(404).send({
+            message: `Cannot delete note with id=${id}. Note was not found!`
+          });
+        } else {
+          res.send({
+            message: "Note was deleted successfully!"
+          });
+        }
+      })
+      .catch(err => {
+        res.status(500).send({
+          message: `Could not delete note with id=${id}.`
+        });
+      });
+  };
